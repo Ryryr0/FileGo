cc16dcd995539205daf2f6dab9cf45a1975ab443
Artyom Korolev <artemkor08@gmail.com>
2025-01-14 18:29:32 +0300

    little changes

--- FileGo/FileGo/settings.py
+++ FileGo/FileGo/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024 * 1024
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
